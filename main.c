00477614620f6aeafd9daf77a471c02395f75d5a
hschoi0416 <hschoi0416@gmail.com>
2026-07-08 21:36:07 +0900

    Refactor: scheduler_start for clean first-task entry, remove first_switch flag

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 #define CLINT_MTIMECMP (CLINT_BASE + 0x4000)
 #define CLINT_MTIME (CLINT_BASE + 0xBFF8)
 
-#define TICK_INTERVAL 1000000UL
+#define TICK_INTERVAL 5000000UL
 #define read_csr(reg) ({ unsigned long __v; \
     asm volatile ("csrr %0, " #reg : "=r"(__v)); __v; })
 #define write_csr(reg, val) ({ \
@@ -108,23 +108,8 @@ uint64_t *trap_dispatch(uint64_t *cur_sp) {
         volatile uint32_t* msip = (volatile uint32_t*)CLINT_MSIP;
         *msip = 0;
         
-        if (first_switch) {
-            first_switch = 0;
-            current_task = 0;
-            uart_puts("S first->0\n");
-            return tasks[0].sp;
-        }
-
-        uart_puts("S switch ");
-        uart_putc('0' + current_task);
-        uart_puts("->");
-
-
         tasks[current_task].sp = cur_sp;
         current_task = (current_task + 1) % MAX_TASKS;
-
-        uart_putc('0' + current_task);
-        uart_putc('\n');
 
         return tasks[current_task].sp;
     }
@@ -139,11 +124,12 @@ void timer_init(void) {
     *mtimecmp = *mtime + TICK_INTERVAL;
     write_csr(mtvec, (unsigned long)trap_entry);
     write_csr(mie, read_csr(mie) | (1 << 7) | (1 << 3));
-    write_csr(mstatus, read_csr(mstatus) | (1 << 3));
+    //write_csr(mstatus, read_csr(mstatus) | (1 << 3));
 
 
 }
 
+extern void scheduler_start(uint64_t* first_sp);
 
 int main(void) {
     uart_puts("hello\n");
@@ -151,6 +137,8 @@ int main(void) {
     timer_init();
     uart_puts("scheduler start\n");
 
+    current_task = 0;
+    scheduler_start(tasks[0].sp);
     while (1) {
     }
     return 0;
